c32dfebbecabca4f0120ef84c455d65bbc324c29
varsha-2530 <vb8607518@gmaail.com>
2025-07-19 15:26:08 +0530

    update

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use('/emp', require('./controllers/routes'));
 
 // Start server
 const PORT = process.env.PORT || 3000;
+    
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
