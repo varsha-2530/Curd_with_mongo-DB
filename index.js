6b6c592d380fa51c4e1efd45dc129eabe530d533
varsha-2530 <vb8607518@gmaail.com>
2025-07-19 15:13:23 +0530

    update-index.js

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const express = require("express");
 const connectDB = require("./models/db");
-const app = express();
+const path = require("path");
+require('dotenv').config();
+
 const Handlebars = require('handlebars');
 const engine = require('express-handlebars').engine;
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
-require('dotenv').config()
 
+const app = express();
 
+// Connect to MongoDB
 connectDB();
 
-const PORT = process.env.PORT || 3000;
-
+// Middleware
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Set Handlebars as view engine
 app.engine('handlebars', engine({
     defaultLayout: 'main',
     handlebars: allowInsecurePrototypeAccess(Handlebars)
 }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
-app.set('view engine', 'handlebars');  
-app.set('views', './views');
-
+// Routes
 app.get('/', (req, res) => {
     res.render('home');
 });
-
 app.use('/emp', require('./controllers/routes'));
 
+// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
