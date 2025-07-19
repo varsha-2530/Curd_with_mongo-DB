ba6a48282936b585106f36913586b6fe67778d27
varsha-2530 <vb8607518@gmaail.com>
2025-07-19 15:00:54 +0530

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 
 connectDB();
 
-const PORT =        process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
